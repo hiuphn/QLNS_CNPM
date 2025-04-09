ef4210c2af7dcc0e444204ab909384658997b50b
Pham Huynh Ngoc Hieu <phamhuynhngochieu468@gmail.com>
2025-04-09 09:32:05 +0700

    done all

---
+++ QuanLyNhanVien/MVVM/View/BangLuongSubView/BangTinhLuongView.xaml.cs
@@ -0,0 +1,162 @@
+﻿using BUS;
+using DTO;
+using Microsoft.Win32;
+using OfficeOpenXml.Style;
+using OfficeOpenXml;
+using QuanLyNhanVien.MessageBox;
+using QuanLyNhanVien.WindowView;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace QuanLyNhanVien.MVVM.View.BangLuongSubView
+{
+    /// <summary>
+    /// Interaction logic for BangTinhLuongView.xaml
+    /// </summary>
+    public partial class BangTinhLuongView : UserControl
+    {
+        BUS_BANGTINHLUONG busBangTinhLuong = new BUS_BANGTINHLUONG();
+        BUS_NHANVIEN busNhanVien = new BUS_NHANVIEN();
+        public BangTinhLuongView()
+        {
+            InitializeComponent();
+            CbxesLoaded();
+        }
+
+        private void bangTinhLuong_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+               
+            }
+        }
+
+        private void lamMoiBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ResetData();
+        }
+
+        private void xuatExcelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = "";
+
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            dialog.Filter = "Excel |*.xlsx";
+
+            if (dialog.ShowDialog() == true)
+            {
+                filePath = dialog.FileName;
+            }
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                bool? result = new MessageBoxCustom("Đường dẫn không hợp lệ!", MessageType.Error, MessageButtons.Ok).ShowDialog();
+                return;
+            }
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            try
+            {
+                using (ExcelPackage p = new ExcelPackage())
+                {
+                    p.Workbook.Properties.Author = "Nhóm13_QLNV";
+                    p.Workbook.Properties.Title = "Bảng lương nhân viên";
+                    p.Workbook.Worksheets.Add("Sheet 1");
+
+                    ExcelWorksheet ws = p.Workbook.Worksheets[0];
+                    ws.Name = "Test sheet";
+                    ws.Cells.Style.Font.Size = 14;
+                    ws.Cells.Style.Font.Name = "Consolas";
+
+                    string[] arrColumnHeader = { "Mã nhân viên", "Lương", "Tháng", "Năm", "Ghi chú" };
+
+                    var countColHeader = arrColumnHeader.Count();
+
+                    ws.Cells[1, 1].Value = "Bảng lương nhân viên";
+                    ws.Cells[1, 1, 1, countColHeader].Merge = true;
+                    ws.Cells[1, 1, 1, countColHeader].Style.Font.Bold = true;
+                    ws.Cells[1, 1, 1, countColHeader].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    int colIndex = 1;
+                    int rowIndex = 2;
+
+                    foreach (var item in arrColumnHeader)
+                    {
+                        var cell = ws.Cells[rowIndex, colIndex];
+
+                        var fill = cell.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(System.Drawing.Color.LightBlue);
+
+                        var border = cell.Style.Border;
+                        border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                        cell.Value = item;
+                        colIndex++;
+                    }
+                    DataTable dt = new DataTable();
+                    
+                    {
+                        dt = busBangTinhLuong.getBangTinhLuong();
+                    }
+
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        colIndex = 1;
+                        rowIndex++;
+
+                        ws.Cells[rowIndex, colIndex++].Value = dr["Mã nhân viên"].ToString();
+                        ws.Cells[rowIndex, colIndex++].Value = dr["Lương"].ToString();
+                        ws.Cells[rowIndex, colIndex++].Value = dr["Tháng"].ToString();
+                        ws.Cells[rowIndex, colIndex++].Value = dr["Năm"].ToString();
+                        ws.Cells[rowIndex, colIndex++].Value = dr["Ghi chú"].ToString();
+
+                    }
+
+                    Byte[] bin = p.GetAsByteArray();
+                    File.WriteAllBytes(filePath, bin);
+
+                }
+                bool? result = new MessageBoxCustom("Xuất excel thành công!", MessageType.Success, MessageButtons.Ok).ShowDialog();
+            }
+            catch
+            {
+                bool? result = new MessageBoxCustom("Đã xảy ra lỗi khi lưu file!", MessageType.Error, MessageButtons.Ok).ShowDialog();
+            }
+        }
+
+        private void numberBoxes_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        public void DataGridLoad()
+        {
+            bangLuongDtg.DataContext = busBangTinhLuong.getBangTinhLuong();
+        }
+
+        public void ResetData()
+        {
+            DataGridLoad();
+        }
+
+        public void CbxesLoaded()
+        {
+           
+        }
+    }
+}
---
+++ QuanLyNhanVien/MVVM/View/TraCuuThongTinView.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BUS;
+using DTO;
+using QuanLyNhanVien.MessageBox;
+using QuanLyNhanVien.WindowView;
+
+namespace QuanLyNhanVien.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for TraCuuThongTinView.xaml
+    /// </summary>
+    public partial class TraCuuThongTinView : UserControl
+    {
+        BUS_NHANVIEN busNhanVien = new BUS_NHANVIEN();
+        public TraCuuThongTinView()
+        {
+            InitializeComponent();
+            DataGridLoad();
+        }
+        public void DataGridLoad()
+        {
+            dsTimKiemThongTinDtg.DataContext = busNhanVien.getNhanVien();
+        }
+
+        private void timkiemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Loc();
+        }
+
+        private void timkiemTbx_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (manvRdBtn.IsChecked == true)
+            {
+                e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+            }
+            if (hotenRdBtn.IsChecked == true)
+            {
+                e.Handled = new Regex("[0-9]+").IsMatch(e.Text);
+            }
+            if (sdtRdBtn.IsChecked == true)
+            {
+                e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+            }
+        }
+
+        private void lamMoiBtn_Click(object sender, RoutedEventArgs e)
+        {
+            timkiemTbx.Text = "";
+            DataGridLoad();
+        }
+
+        private void timkiemTbx_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Loc();
+            }    
+        }
+
+        public void Loc()
+        {
+            if (timkiemTbx.Text == String.Empty)
+            {
+                bool? result = new MessageBoxCustom("Vui lòng điền đầy đủ thông tin!", MessageType.Warning, MessageButtons.Ok).ShowDialog();
+                DataGridLoad();
+                return;
+            }
+            if (manvRdBtn.IsChecked == true)
+            {
+                dsTimKiemThongTinDtg.DataContext = busNhanVien.TimKiemNhanVienTheoMa(timkiemTbx.Text);
+            }
+            if (hotenRdBtn.IsChecked == true)
+            {
+                dsTimKiemThongTinDtg.DataContext = busNhanVien.TimKiemNhanVienTheoTen(timkiemTbx.Text);
+            }
+            if (sdtRdBtn.IsChecked == true)
+            {
+                dsTimKiemThongTinDtg.DataContext = busNhanVien.TimKiemNhanVienTheoSDT(timkiemTbx.Text);
+            }
+        }
+
+        public void XemChiTiet()
+        {
+            DTO_NHANVIEN ctNhanVien = new DTO_NHANVIEN();
+            DataRowView row = dsTimKiemThongTinDtg.SelectedItem as DataRowView;
+            ChiTietNhanVienForm chiTietNhanVienForm = new ChiTietNhanVienForm();
+
+            ctNhanVien.Manv = int.Parse(row[0].ToString());
+            ctNhanVien.Maphong = row[1].ToString();
+            ctNhanVien.Maluong = row[2].ToString();
+            ctNhanVien.Hoten = row[3].ToString();
+            ctNhanVien.Ngaysinh = DateTime.Parse(row[4].ToString());
+            ctNhanVien.Gioitinh = row[5].ToString();
+            ctNhanVien.Dantoc = row[6].ToString();
+            ctNhanVien.Cmnd_cccd = row[7].ToString();
+            ctNhanVien.Noicap = row[8].ToString();
+            ctNhanVien.Chucvu = row[9].ToString();
+            ctNhanVien.Maloainv = row[10].ToString();
+            ctNhanVien.Loaihd = row[11].ToString();
+            ctNhanVien.Thoigian = int.Parse(row[12].ToString());
+            ctNhanVien.Ngaydangki = DateTime.Parse(row[13].ToString());
+            ctNhanVien.Ngayhethan = DateTime.Parse(row[14].ToString());
+            ctNhanVien.Sdt = row[15].ToString();
+            ctNhanVien.Hocvan = row[16].ToString();
+            ctNhanVien.Ghichu = row[17].ToString();
+
+            chiTietNhanVienForm.ctNhanVien = ctNhanVien;
+            chiTietNhanVienForm.ShowDialog();
+            DataGridLoad();
+        }
+
+        private void dsTimKiemThongTinDtg_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dsTimKiemThongTinDtg.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            XemChiTiet();
+        }
+    }
+}
